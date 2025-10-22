5bf80401b06778b3e9db30122b60773e1a8c7944
Ingrid Bini <binifideli@gmail.com>
2025-10-22 14:25:17 -0300

    first commit

---
+++ src/main/java/br/com/bini/buscadorClimatico/BuscadorClimaticoApplication.java
@@ -0,0 +1,13 @@
+package br.com.bini.buscadorClimatico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscadorClimaticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscadorClimaticoApplication.class, args);
+	}
+
+}
